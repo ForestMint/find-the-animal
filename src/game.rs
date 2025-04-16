9e1bafb7080551220157c7f5d9560f9e12aef91b
desdames-nicolas <n.desdames@gmail.com>
2025-04-16 21:26:01 +0200

    is_over added to Game

--- src/game.rs
+++ src/game.rs
@@ -2,12 +2,17 @@ mod pool_of_animals;
 
 pub struct Game {
     pool_of_animals: pool_of_animals::PoolOfAnimals,
+    is_over: bool,
 }
 
 impl Game {
 
     pub fn new() -> Game {
-        Game {pool_of_animals: pool_of_animals::PoolOfAnimals::fill()}
+        Game {pool_of_animals: pool_of_animals::PoolOfAnimals::fill(), is_over: false}
     }
-    
+
+    pub fn is_over(self) -> bool {
+        self.is_over
+    }
+
 }

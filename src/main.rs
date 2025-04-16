950b60fcc991b63931f000c6958366f25004a289
desdames-nicolas <n.desdames@gmail.com>
2025-04-16 23:27:11 +0200

    program asks for my name instead of the name of the animal to shoot

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,33 @@
+use std::io::stdin;
+
 mod game;
 
-fn request_shoot() -> String {
-    "walrus".to_string()
-}
 
 fn main () {
     let my_game = game::Game::new();
 
     while !&my_game.is_over() {
-        let shoot_candidate = request_shoot();
+
+        let mut line = String::new();
+        println!("Enter your name :");
+        let b1 = std::io::stdin().read_line(&mut line).unwrap();
+        println!("Hello , {}", line);
+        println!("no of bytes read , {}", b1);
+
+
+
+
+
+        let shoot_candidate = "walrus".to_string();
+
+
+
+
+
         game::Game::shoot_animal(&my_game, shoot_candidate);
+
+
+
+
     }
 }

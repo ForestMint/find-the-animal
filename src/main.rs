017dddadc7265ef1f4004d01e03bf4ac2bbb7619
desdames-nicolas <n.desdames@gmail.com>
2025-04-21 19:05:08 +0200

    coments added in main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,16 @@ mod game;
 fn main () {
     let my_game = game::Game::new();
 
+    // as long as the game is not over
     while !&my_game.is_over() {
 
+        // ask the player for an animal
         let mut shoot_candidate = String::new();
         println!("Animal to eliminate : ");
         let b1 = std::io::stdin().read_line(&mut shoot_candidate).unwrap();
 
-
+        // shoot the animal chosen
         game::Game::shoot_animal(&my_game, shoot_candidate);
-
-
-
 
     }
 }
